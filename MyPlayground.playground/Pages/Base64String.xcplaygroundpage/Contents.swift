3f3f858b1e90be37c309813a3c530d3dee2391db
Tiz <kshitiz.ghimrie@gmail.com>
2018-12-09 10:42:18 +1100

    base 64 encoding - decoding

--- MyPlayground.playground/Pages/Base64String.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Base64String.xcplaygroundpage/Contents.swift
@@ -4,5 +4,7 @@ import Foundation
 
 var str = "Hello, playground"
 
-str.data(using: .utf8)?
+let base64EncodedString = str.data(using: .utf8)?
     .base64EncodedString()
+
+let string = String(data:Data(base64Encoded: base64EncodedString!)!, encoding: .utf8)
