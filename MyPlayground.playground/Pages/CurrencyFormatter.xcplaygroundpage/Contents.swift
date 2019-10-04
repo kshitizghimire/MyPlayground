5910259c3a10107005e1eb5a18b91978a32d4268
Tiz <kshitiz.ghimrie@gmail.com>
2019-10-04 20:51:49 +1000

    cleaning

---
+++ MyPlayground.playground/Pages/CurrencyFormatter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct DefaultCurrencyFormatter {
+    private let formatter: NumberFormatter
+
+    init(locale: Locale) {
+        formatter = NumberFormatter()
+        formatter.locale = locale
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+    }
+
+    func formatCurrency(string: String) -> String? {
+        guard let doubleValue = Double(string) else {
+            return nil
+        }
+        return formatter.string(from: NSNumber(value: doubleValue))
+    }
+}
+
+
+let rawCurrencyString = "12.313"
+let locale = Locale(identifier: "en_AU")
+
+let formatter = DefaultCurrencyFormatter(locale: locale)
+formatter.formatCurrency(string: rawCurrencyString)
+
---
+++ MyPlayground.playground/Pages/NSRegularExpression.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let testString = """
+The cat
+sat on
+the mat
+"""
+
+public struct RegularExpression: ExpressibleByStringLiteral {
+    public let pattern: String
+    private let regularExpression: NSRegularExpression
+
+    init(_ pattern: String) {
+        self.pattern = pattern
+        do {
+            self.regularExpression = try NSRegularExpression(pattern: pattern)
+        } catch {
+            preconditionFailure("Illegal regular expression pattern: \(pattern).")
+        }
+    }
+
+    public init(stringLiteral value: String) {
+        self.init(value)
+
+    }
+
+    public func matches(_ string: String) -> Bool {
+        return self.regularExpression.firstMatch(in: string, options: [], range: NSRange(string.startIndex..., in: string)) != nil
+    }
+}
+
+
+
+let regularExpression: RegularExpression = "the"
+regularExpression.matches(testString)
