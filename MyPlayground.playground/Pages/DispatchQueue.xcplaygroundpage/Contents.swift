8a4e2460c29496f26497e3114bb9dd0d91f9c6a1
Tiz <kshitiz.ghimire@gmail.com>
2020-03-15 20:17:27 +1100

    dispatch queue

--- MyPlayground.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
@@ -1,25 +1,43 @@
 import Foundation
 
-let serialQueue = DispatchQueue(label: "com.queue.Serial", attributes: .concurrent)
-for i in 1...5 {
-    serialQueue.async {
-        print(Thread.isMainThread)
-        print("\(i)")
+let serialQueue = DispatchQueue(label: "com.queue.Serial")
+serialQueue.async {
+        print("Task1 started")
+        print("is main thread: \(Thread.isMainThread)")
         sleep(2)
-    }
-}
-
-
-class AAA {
-    let p: String
-    static var bbb: AAA {
-        return AAA(p: "P")
-    }
+        print("Task1 ended")
     
-    init(p: String) {
-        self.p = p
-    }
-   
 }
+serialQueue.async {
+        print("Task2 started")
+        print("is main thread: \(Thread.isMainThread)")
+        sleep(2)
+        print("Task2 ended")
+    
+}
+
+let cuncurrentQueue = DispatchQueue(label: "com.queue.Serial", attributes: .concurrent)
+cuncurrentQueue.async {
+        print("Task1 started")
+        print("is main thread: \(Thread.isMainThread)")
+        sleep(2)
+        print("Task1 ended")
+    
+}
+cuncurrentQueue.async {
+        print("Task2 started")
+        print("is main thread: \(Thread.isMainThread)")
+        sleep(2)
+        print("Task2 ended")
+    
+}
+cuncurrentQueue.async {
+        print("Task3 started")
+        print("is main thread: \(Thread.isMainThread)")
+        sleep(2)
+        print("Task3 ended")
+    
+}
+
 
 
