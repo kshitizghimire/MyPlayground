8a57f60a14eeb6fee40f4544f79f1c853953bdd3
Tiz <kshitiz.ghimire@gmail.com>
2020-07-08 00:23:20 +1000

    add clock

---
+++ MyPlayground.playground/Pages/Clock.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+for i in TimeZone.knownTimeZoneIdentifiers {
+    let t = TimeZone(identifier: i)
+    print(t!,t!.abbreviation()!)
+    
+    let currentDate = Date()
+    let format = DateFormatter()
+    format.timeZone = t
+    format.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    let dateString = format.string(from: currentDate)
+    print(dateString )
+   
+}
