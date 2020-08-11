3e9cdffffdf8e55b94371417b3c7a265e1c41450
Tiz <kshitiz.ghimire@gmail.com>
2020-08-11 21:56:07 +1000

    Block based notification

---
+++ MyPlayground.playground/Pages/BlockNotificationCenter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+final class BlockNotificationCenter {
+    
+    var action: (() -> Void)? = nil
+
+    func addNotification(name: Notification.Name) {
+        NotificationCenter.default.addObserver(self, selector: #selector(foo), name: name, object: nil)
+    }
+    
+    @objc
+    func foo() {
+        action?()
+    }
+}
+
+//: [Next](@next)
+
+
+let blockN = BlockNotificationCenter()
+blockN.action = {
+    print("here")
+}
+blockN.addNotification(name: Notification.Name(rawValue: "Foo"))
+
+NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: "Foo")))
+NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: "Foo")))
+
--- MyPlayground.playground/Pages/Try!.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Try!.xcplaygroundpage/Contents.swift
@@ -2,23 +2,8 @@
 
 import Foundation
 
-class Solution {
-    func groupAnagrams(_ strs: [String]) -> [[String]] {
-        
-        var result = [[Character]: [String]]()
-        for str in strs {
-            let sorted = str.sorted()
-            if var array = result[sorted] {
-                array.append(str)
-                result[sorted] = array
-            } else {
-                result[sorted] = [str]
-            }
-            
-        }
+let a = DispatchTime.now()
+let b = DispatchTime.now()
+print(b.uptimeNanoseconds-a.uptimeNanoseconds)
 
-        return Array(result.values)
-    }
-}
-
-Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+CFAbsoluteTimeGetCurrent
