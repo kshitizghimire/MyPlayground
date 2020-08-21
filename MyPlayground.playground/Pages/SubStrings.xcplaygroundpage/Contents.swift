b030ed9a2469eed0f1c0ed93e92dc60ef74d9d44
Tiz <kshitiz.ghimire@gmail.com>
2020-08-21 22:04:26 +1000

    add substring using String.Index

---
+++ MyPlayground.playground/Pages/SubStrings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let string = "abcdef"
+
+var subString = [Substring]()
+
+var i = string.startIndex
+while i < string.endIndex {
+    var j = i
+    while j < string.endIndex {
+        subString.append(string[i...j])
+        j = string.index(after: j)
+    }
+    i = string.index(after: i)
+}
+
+print(subString)
