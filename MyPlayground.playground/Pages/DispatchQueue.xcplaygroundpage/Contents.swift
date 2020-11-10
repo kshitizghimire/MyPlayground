d3ae06ef70b37aca00b4b976ff35f4146cae485d
Tiz <kshitiz.ghimire@gmail.com>
2020-11-10 22:30:46 +1100

    update dispatch queue names

--- MyPlayground.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
@@ -15,25 +15,25 @@ serialQueue.async {
         print("Task2 ended")
 }
 
-let cuncurrentQueue = DispatchQueue(label: "com.queue.Serial", attributes: .concurrent)
+let cuncurrentQueue = DispatchQueue(label: "com.queue.Concurrent", attributes: .concurrent)
 cuncurrentQueue.async {
-        print("Task1 started")
-        print("is main thread: \(Thread.isMainThread)")
+    print("\(cuncurrentQueue.label) Task1 started")
+        print("\(cuncurrentQueue.label) is main thread: \(Thread.isMainThread)")
         sleep(2)
-        print("Task1 ended")
+        print("\(cuncurrentQueue.label) Task1 ended")
     
 }
 cuncurrentQueue.async {
-        print("Task2 started")
-        print("is main thread: \(Thread.isMainThread)")
+        print("\(cuncurrentQueue.label) Task2 started")
+        print("\(cuncurrentQueue.label) is main thread: \(Thread.isMainThread)")
         sleep(2)
-        print("Task2 ended")
+        print("\(cuncurrentQueue.label) Task2 ended")
 }
 cuncurrentQueue.async {
-        print("Task3 started")
-        print("is main thread: \(Thread.isMainThread)")
+        print("\(cuncurrentQueue.label) Task3 started")
+        print("\(cuncurrentQueue.label) is main thread: \(Thread.isMainThread)")
         sleep(2)
-        print("Task3 ended")
+        print("\(cuncurrentQueue.label) Task3 ended")
 }
 
 
