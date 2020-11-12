e38118eda05c941b093018f0231fb696a4337444
Tiz <kshitiz.ghimire@gmail.com>
2020-11-12 17:03:55 +1100

    add flat map implemenation

--- MyPlayground.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -1,5 +1,4 @@
 
-
 extension Sequence {
     public func map2<T>( _ transform: (Element) -> T) -> [T] {
         var result = [T]()
@@ -11,5 +10,27 @@ extension Sequence {
     }
 }
 
-let m = [2].map2 { String($0) }
+let m = [2,4,6,3,5].map2 { (num) -> String in
+    return String(num)
+}
 print(m)
+
+extension Sequence {
+    public func flatMap2<T: Sequence>( _ transform: (Element) -> T) -> [T.Element] {
+        var result = [T.Element]()
+        for element in self {
+            result += transform(element)
+        }
+        return result
+    }
+}
+
+let cm = [[2,4],[6],[3,5]].flatMap2 { $0 }
+print(cm)
+
+let nums = [
+    0...19,
+    40...50
+]
+let allNums = nums.flatMap2 { $0 }
+print(allNums)
