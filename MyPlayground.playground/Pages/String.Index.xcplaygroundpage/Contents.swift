36213a8a0a64f94206bf539e30a54beae0ea9fb5
Tiz <kshitiz.ghimire@gmail.com>
2020-11-29 09:23:45 +1100

    add for in using stirng.indices

--- MyPlayground.playground/Pages/String.Index.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/String.Index.xcplaygroundpage/Contents.swift
@@ -4,10 +4,16 @@ import Foundation
 
 let string = "a👨‍👩‍👧‍👦bcd"
 
+for index in string.indices {
+    print("Character: \(string[index])")
+}
+
 let secondIndex = string.index(after: string.startIndex)
 let thirdIndex = string.index(string.startIndex, offsetBy: 2)
 let lastIndex = string.index(before: string.endIndex)
 
 print(string[secondIndex])
 print(string[thirdIndex])
-print(string[lastIndex]) 
+print(string[lastIndex])
+
+
