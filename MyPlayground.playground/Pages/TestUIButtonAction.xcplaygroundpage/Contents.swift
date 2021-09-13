f09f94f82e2f27f24e04b05a6bccd9e9e4e90ff1
Tiz <kshitiz.ghimire@gmail.com>
2021-09-13 23:06:31 +1000

    Add UIButton action test

---
+++ MyPlayground.playground/Pages/TestUIButtonAction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+import XCTest
+
+final class ViewController: UIViewController {
+    private(set) lazy var addButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(tapAddButton), for: .touchUpInside)
+  
+        return button
+    }()
+    
+    // MARK: - Actions
+      var didTapAdd: (() -> Void)?
+      @objc private func tapAddButton() {
+          didTapAdd?()
+      }
+}
+
+
+final class ViewControllerTests: XCTestCase {
+    
+    func testAction() {
+        let expectation = expectation(description: #function)
+        let viewController = ViewController()
+        viewController.didTapAdd = {
+            expectation.fulfill()
+        }
+        
+        viewController.addButton.sendActions(for: .touchUpInside)
+        
+        wait(for: [expectation], timeout: 0.1)
+        
+    }
+}
+
+ViewControllerTests.defaultTestSuite.run()
